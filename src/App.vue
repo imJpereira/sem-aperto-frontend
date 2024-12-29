eb622917ac1af17bd179d5bd8b0997148b31f627
João <jps.pereira0805@gmail.com>
2024-12-29 15:18:59 -0300

    removing: files from the vue project

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,74 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView, useRoute } from 'vue-router'
+
+const route = useRoute();
+
+const isActive = (path) => {
+  return path === route.path;
+}
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+  <main>
+    <aside class="main-menu">
+      <div class="main-menu-profile">
+        <h1>Nome</h1>
+      </div>
+      <nav class="main-menu-content">
+        <ul>
+          <li :class="{ menuActive: isActive('/despesas') }">
+            <RouterLink to="/despesas" >Despesas</RouterLink
+            >
+          </li>
+          <li :class="{ menuActive: isActive('/planos') }">
+            <RouterLink to="/planos" 
+              >Planos</RouterLink
+            >
+          </li>
+        </ul>
       </nav>
-    </div>
-  </header>
+    </aside>
 
-  <RouterView />
+    <div class="tab-container">
+      <RouterView />
+    </div>
+
+
+  </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+.main-menu {
+  width: 20rem;
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
+  color: var(--vt-c-white);
+  background-color: var(--color-main-theme);
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+  & a {
+    font-size: 20px;
+    color: var(--vt-c-text-dark-2);
+    text-decoration: none;
   }
 }
+
+.main-menu-content {
+  padding: 2rem 1.5rem;
+
+  li {
+    margin-bottom: 1rem;
+    position: relative;
+  }
+}
+
+.main-menu-profile {
+  padding: 5rem 1.5rem;
+  border-bottom: 2px solid var(--vt-c-divider-dark-2);
+}
+
+.menuActive a {
+  color: var(--vt-c-white);
+}
+
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import DespesasView from '../views/DespesasView.vue'
+import PlanosView from '@/views/PlanosView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      redirect: '/planos',
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/despesas',
+      name: 'despesas',
+      component: DespesasView,
+    },
+    {
+      path: '/planos',
+      name: 'planos',
+      component: PlanosView,
     },
   ],
 })
